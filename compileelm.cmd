0435eee63b5fd7127f52068af0313f72f18502c0
JonathanDDuncan <duncanjonathan@yahoo.ca>
2017-11-11 14:57:18 -0600

    Add elm app to application

---
+++ compileelm.cmd
@@ -0,0 +1,1 @@
+elm-make src/elm/Main.elm --output=src/elm/main.js
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,38 @@ import { ListPage } from '../pages/list/list';
 
 import { ContentPage } from '../pages/content/content';
 
+
 import { RXDBConsejoComponent } from '../pages/rxdbconsejo/rxdbconsejo.component';
 import { RXDBPublicadorSampleComponent } from './../pages/rxdbpublicadorsample/rxdbpublicadorsample.component';
 import { RXDBSemanaComponent } from './../pages/rxdbsemana/rxdbsemana.component';
 import { ScaffoldingPage } from '../pages/scaffolding/scaffolding';
+declare var require: any;
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  
+
   @ViewChild(Nav) nav: Nav;
   rootPage: any = HomePage;
-  
-  pages: Array<{title: string, component: any}>;
-  
-    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-      this.initializeApp();
-  
-      // used for an example of ngFor and navigation
-      this.pages = [
-        { title: 'Home', component: HomePage },
-        { title: 'List', component: ListPage },
-        { title: 'Publicadores', component: RXDBPublicadorSampleComponent  },
-        { title: 'Consejos', component: RXDBConsejoComponent  },
-        { title: 'Semanas', component: RXDBSemanaComponent  },
-        { title: 'Content', component: ContentPage  },
-        { title: 'ScaffoldingPage', component: ScaffoldingPage  },
-        
-    
-      ];
-    }
+
+  pages: Array<{ title: string, component: any }>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage },
+      { title: 'Publicadores', component: RXDBPublicadorSampleComponent },
+      { title: 'Consejos', component: RXDBConsejoComponent },
+      { title: 'Semanas', component: RXDBSemanaComponent },
+      { title: 'Content', component: ContentPage },
+      { title: 'ScaffoldingPage', component: ScaffoldingPage },
+
+
+    ];
+  }
 
 
   initializeApp() {
@@ -54,5 +56,9 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+  ngAfterContentInit() {
+    const Elm = require('../elm/main.js');
+    Elm.Main.embed(document.getElementById('elm'));
   }
 }
